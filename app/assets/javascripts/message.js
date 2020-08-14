24670539560c607806ae3056143d6dea195bf5f2
kudokaisei <kudokaisei@gmail.com>
2020-08-14 12:52:49 +0900

    json-api

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,44 @@
 $(function(){
+  function buildMessage(message){
+  if(message.image) {
+    let html =
+     `<div class="message-field">
+        <div class="message-field__view">
+          <div class="message-field__view__name">
+            ${message.user_name}
+          </div>
+          <div class="message-field__view__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message-field__message">
+          <p class="message-field__message__text">
+            ${message.content}          
+          </p>
+          <img class="message-field__message__image" src="${message.image}">
+        </div>
+      </div>`
+    return html;
+  } else {
+    let html =
+    `<div class="message-field">
+      <div class="message-field__view">
+        <div class="message-field__view__name">
+          ${message.user_name}
+        </div>
+        <div class="message-field__view__date">
+          ${message.created_at}
+        </div>
+      </div>
+      <div class="message-field__message">
+        <p class="message-field__message__text">
+          ${message.content}          
+        </p>
+      </div>
+    </div>`
+  };
+} 
+
   $('.form').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this)
@@ -10,6 +50,9 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(message){
+      let html = buildMessage(message)
     })
   })
 })
